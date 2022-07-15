b92c0c0bba45ef344bb5b9604d03a107d1dfdbdf
Brijeshsah16 <brijesh.sah16121999@gmail.com>
2022-07-15 12:00:44 +0530

    this is the first commit

---
+++ lib/main.dart
@@ -0,0 +1,116 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:dialog_flowtter/dialog_flowtter.dart';
+
+import 'Messages.dart';
+void main()
+{
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'CHATBOT',
+      theme:ThemeData(
+        brightness: Brightness.dark
+      ) ,
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  late DialogFlowtter dialogFlowtter;
+  final TextEditingController _controller = TextEditingController();
+  List<Map<String,dynamic>> messages=[];
+
+  @override
+  void initState() {
+    DialogFlowtter.fromFile().then((instance) => dialogFlowtter=instance);
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+        title: Text('brijesh sah'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: MessagesScreen(messages: messages)),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 14,vertical: 8),
+              color: Colors.deepPurple,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: TextField(
+                        controller: _controller,
+                        style: TextStyle(
+                          color: Colors.white,
+
+                        ),
+
+                      )
+                  ),
+                  IconButton(
+                      onPressed: (){
+                        sendMessage(_controller.text);
+                        _controller.clear();
+                      },
+                      icon: Icon(Icons.send),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  sendMessage(String text) async{
+    if(text.isEmpty)
+      {
+        print('message is empty');
+      }
+    else
+      {
+          setState(() {
+            addMessage(Message(text: DialogText(text: [text])),true);
+          }
+          );
+
+          DetectIntentResponse response= await dialogFlowtter.detectIntent(queryInput: QueryInput(text:TextInput(text: text)));
+
+          if(response.message==null)  return ;
+
+          setState((){
+            addMessage(response.message!);
+          });
+      }
+  }
+  addMessage(Message message,[bool isUserMessage = false ])
+  {
+        messages.add({
+          'message': message,
+          'isUserMessage': isUserMessage
+        }
+        );
+  }
+}
